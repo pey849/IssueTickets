ab65af0900569487198019169a175aa545f0b106
Peter Yeung <peter.yeung@usask.ca>
2018-07-11 23:10:46 -0600

    added a slug url variable into the Tickets class and had to migrate the changes

--- TrackIT/tickets/admin.py
+++ TrackIT/tickets/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Tickets
 # Register your models here.
+#add the Tickets model to the admin console page
+admin.site.register(Tickets)
--- TrackIT/tickets/models.py
+++ TrackIT/tickets/models.py
@@ -6,11 +6,16 @@ from django.db import models
 # Each model maps to a single table in a datebase
 # field types: https://docs.djangoproject.com/en/2.0/ref/models/fields/
 
+# if you make any new vairables in the class, and have preexiting data, set a default
+# then run the command "python manage.py makemigrations" and "python manage.py migrate"
+# then start the server
+
 class Tickets(models.Model):
     #no more than 100 characters for title
     title = models.CharField(max_length=100)
     issueDescription = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=50, default="")
     #sender
 
     def __str__(self):
