3d69f14ab3d5e09857d1f966f0f0417aa2f78da0
Peter Yeung <peter.yeung@usask.ca>
2018-07-14 17:59:55 -0600

    Extending templates to have a basic header and footer and be able to insert 'blocks' of unique content into the base template layout

--- TrackIT/TrackIT/settings.py
+++ TrackIT/TrackIT/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/helperfiles/'
 
 STATICFILES_DIRS = (
 #JOIN TO PARTS OF A PATH TOGETHER
