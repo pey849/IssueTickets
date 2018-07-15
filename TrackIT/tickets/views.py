8829ab608823902e72d61b2ef202e7b761ff6e2a
Peter Yeung <peter.yeung@usask.ca>
2018-07-14 20:12:38 -0600

    create an template page for displaying information regarding a single issue ticket when clicking on a ticket from our list of tickets

--- TrackIT/tickets/views.py
+++ TrackIT/tickets/views.py
@@ -7,9 +7,15 @@ from django.http import HttpResponse
 def ticket_list(request):
     #grabs all objects from the database of Tickets
     #order by the date field we set in models.py
-    # send it to the view as a dictionary
-    tickets = Tickets.objects.all().order_by('date')
-    return render(request, 'tickets/ticket_list.html', {'tickets':tickets})
+    # send it to the view as a dictionary called myTickets
+    tickets_to_give = Tickets.objects.all().order_by('date')
+    return render(request, 'tickets/ticket_list.html', {'myTickets':tickets_to_give})
 
 def ticket_details_views(request, slug):
-    return HttpResponse(slug)
+    #return HttpResponse(slug)
+
+    #query our database for a certain field we are looking for and place the object
+    #we found from our query into a veriable
+    #Ticket.object.get(field_tobase_search_on = varaible_passed_in)
+    single_ticket = Tickets.objects.get(slug=slug)
+    return render(request, 'tickets/ticket_details.html', {'single_ticket_details':single_ticket})
