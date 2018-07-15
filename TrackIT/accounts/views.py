90d57d6f8879b0ec8aac569bac44a2e7ee64322e
Peter Yeung <peter.yeung@usask.ca>
2018-07-14 23:37:19 -0600

    created simple Form submission in signup.html

--- TrackIT/accounts/views.py
+++ TrackIT/accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+#import django's already defined form
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    #store django's prebuilt form in a variable
+    myForm = UserCreationForm()
+    #send to the signup.html page the variable 'accountForm'
+    return render(request,'accounts/signup.html', {'accountForm':myForm})
