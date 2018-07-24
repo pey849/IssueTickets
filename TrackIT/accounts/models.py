eeb83d0f076176edb5d78c2eefc1965419eada57
Peter Yeung <peter.yeung@usask.ca>
2018-07-23 22:49:31 -0600

    Revert "Created a foriegn key inside of AccountUsers to point to the different roles a person could be"
    This reverts commit 4de43bd91211d8eed42d9c0ffb01b05c8a4be73f.

--- TrackIT/accounts/models.py
+++ TrackIT/accounts/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 #import our database of users that were eitehr create in the admin page or through our signup page
 from django.contrib.auth.models import User
-from roles.models import Roles
 
 # Create your models here.
 #create Account User roles. Are they a client, agent or both
 class AccountUser(models.Model):
-
     trackITUserName = models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
-    #Foreign key to point to different roles a user can be
-    trackITRoleType = models.ForeignKey(Roles,on_delete=models.DO_NOTHING,default=None, null=True, blank=True)
+    isTrackITAgent = models.BooleanField(default=False)
+    isTrackITClient = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.trackITUserName)
